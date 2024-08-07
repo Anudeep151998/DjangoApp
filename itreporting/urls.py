ac8d5436c7aaec2ad3b24db3b4a94cc123db8c24
Anudeep151998 <anudeepburra18@gmail.com>
2024-08-07 21:36:58 +0530

    Initial Commit

---
+++ itreporting/urls.py
@@ -0,0 +1,13 @@
+from . import views 
+from django.urls import path, include
+
+
+app_name = 'itreporting' 
+
+
+urlpatterns = [ 
+
+    path('', views.home, name = 'home'), 
+    path('about/', views.about, name='about'),
+    path('contact/', views.contact, name='contact'),
+]
